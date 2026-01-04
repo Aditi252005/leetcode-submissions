227445e249bd2c85fd8f064139348b6366d8eb2d
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-01-04 14:45:42 +0530

    Time: 93 ms (22.32%), Space: 301.6 MB (40.77%) - LeetHub

--- 3796-find-maximum-value-in-a-constrained-sequence/3796-find-maximum-value-in-a-constrained-sequence.cpp
+++ 3796-find-maximum-value-in-a-constrained-sequence/3796-find-maximum-value-in-a-constrained-sequence.cpp
@@ -30,25 +30,25 @@ public:
         // while(!q.empty()){
         //     auto it=q.front();
         //     q.pop();
-        //     int idx=it.first;//3
-        //     int maxv=it.second;//2
-        //     a[idx]=maxv;
-        //     int i=idx-1;//2
-        //     int j=idx+1;//4
-        //     while(i>=1){
-        //         a[i]=min(a[i],max(abs(diff[i]-a[i+1]),diff[i]+a[i+1]));
+        //     int idx=it.first;//1
+        //     int maxv=it.second;//15
+        //     a[idx]=min(a[idx],maxv);
+        //     int i=idx-1;//0
+        //     int j=idx+1;//2
+        //     while(i>=0){
+        //         a[i]=min(a[i],diff[i]+a[i+1]);
         //         i--;
         //     }
         //     while(j<n){
-        //        a[j]=min(a[j],max(abs(diff[j-1]-a[j-1]),diff[j-1]+a[j-1])); 
+        //        a[j]=min(a[j],diff[j-1]+a[j-1]); 
         //        j++;
         //     }    
         // }
 
         int maxv=0;
         for(int i=0;i<n;i++){
+            //cout<<a[i];
             maxv=max(maxv,a[i]);
-            cout<<a[i]<<" ";
         }
         return maxv;
     }
