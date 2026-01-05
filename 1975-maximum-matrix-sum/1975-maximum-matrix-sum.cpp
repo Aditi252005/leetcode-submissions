f212b542e3d49ac3c78f43080ebb35b34ad52025
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-01-05 19:49:00 +0530

    Time: 0 ms (100%), Space: 39.1 MB (61.32%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<int> s;
         long long sum=0;
         int mine=INT_MAX;
+        int neg=0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]<0) s.push_back(-matrix[i][j]);
-                else{
-                    sum+=matrix[i][j];
-                    mine=min(mine,matrix[i][j]);
-                }
+                if(matrix[i][j]<0) neg++;
+                sum+=abs(matrix[i][j]);
+                mine=min(mine,abs(matrix[i][j]));
+                
             }
         }
         
-        sort(s.begin(),s.end());
-        if(s.size()%2==1){
-            if(s[0]>mine) sum=sum-2*mine+accumulate(s.begin(),s.end(),0LL);
-            else sum=sum+accumulate(s.begin()+1,s.end(),0LL)-s[0];
-        }else{
-            sum=sum+accumulate(s.begin(),s.end(),0LL);
+       
+        if(neg%2==1){
+            sum=sum-2*mine;
         }
 
         return sum;
