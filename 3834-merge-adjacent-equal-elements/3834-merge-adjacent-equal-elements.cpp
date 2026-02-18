4ba40b78afbd2a780b942f9f25abcd55f5574ba2
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-02-19 02:37:30 +0530

    Time: 35 ms (31.41%), Space: 211.5 MB (26.66%) - LeetHub

--- 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
@@ -3,27 +3,24 @@ public:
     vector<long long> mergeAdjacent(vector<int>& nums) {
         int n=nums.size();
         vector<long long> a;
-        //a.push_back(nums[0]);
+        stack<long long> st;
         
         int j=0;
         while(j<n){
-            a.push_back(nums[j]);  //2 2
-            int m=a.size();  //2
-            if(m>1){
-                long long  b=a[m-1];  //2
-                long long c=a[m-2];  //2
-                while(m>1 && b==c){
-                    a.pop_back();
-                    a.pop_back();
-                    a.push_back(2*b); //4
-                    m--; //1
-                    if(m<=1) break;
-                    b=a[m-1];
-                    c=a[m-2];  
-                }
+            long long p=nums[j];
+            while(!st.empty() && st.top()==p){
+                st.pop();
+                p=2*p;                
             }
+            st.push(p);
             j++;
         }
+        while(!st.empty()){
+            a.push_back(st.top());
+            st.pop();
+        }
+        reverse(a.begin(),a.end());
+
         return a;
     }
 };
