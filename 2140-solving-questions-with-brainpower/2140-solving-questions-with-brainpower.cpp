a21d7fdff38af663849719e2572182c59e50257b
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-03-17 00:24:31 +0530

    Time: 16 ms (13.74%), Space: 127.5 MB (33.69%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    long long f(vector<vector<int>>&q,int i,vector<long long>&dp){
+    long long f(int i,vector<vector<int>>&q,vector<long long>&dp){
         int n=q.size();
         if(i>n-1) return 0;
         if(dp[i]!=-1) return dp[i];
 
-        long long take=q[i][0]+f(q,i+q[i][1]+1,dp);
-        long long notTake=f(q,i+1,dp);
+        long long take=q[i][0]+f(i+q[i][1]+1,q,dp);
+        long long skip=f(i+1,q,dp);
 
-        return dp[i]=max(take,notTake);
+        return dp[i]=max(take,skip);
     }
     long long mostPoints(vector<vector<int>>& questions) {
         int n=questions.size();
-        vector<long long>dp(n+1,-1);
-
-        return f(questions,0,dp);
+        vector<long long> dp(n,-1);
+        return f(0,questions,dp);
     }
 };
