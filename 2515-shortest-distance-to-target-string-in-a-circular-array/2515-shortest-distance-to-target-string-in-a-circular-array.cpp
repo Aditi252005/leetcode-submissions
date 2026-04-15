87eed4a96de81a5f27036d5ab35ed124de6210c0
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-04-15 07:19:14 +0530

    Time: 0 ms (100%), Space: 18.4 MB (9.44%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -3,19 +3,9 @@ public:
     int closestTarget(vector<string>& words, string t, int idx) {
         int n=words.size();
         int ans=INT_MAX;
-        int c=(idx+1)%n;
-
-        int i=idx,j=(idx+1)%n;
-        int a=0,b=1;
-
-        while(i!=c){ 
-            if(words[i]==t) ans=min(ans,a);
-            if(words[j]==t) ans=min(ans,b);
-
-            i=(i-1+n)%n;
-            j=(j+1)%n;
-            a++;
-            b++;
+        
+        for(int i=0;i<n;i++){
+            if(words[i]==t) ans=min({ans,abs(i-idx),abs(n-abs(i-idx))});
         }
         if(ans==INT_MAX) return -1;
         return ans;
