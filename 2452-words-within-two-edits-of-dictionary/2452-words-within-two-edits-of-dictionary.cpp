c252b697968a9a190b6422d8553d8b17e944384e
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-04-22 20:26:06 +0530

    Time: 31 ms (9.63%), Space: 20.4 MB (26.2%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        int n=queries.size();
-        int m=dictionary.size();
-        vector<string> ans;
+    vector<string> twoEditWords(vector<string>& q, vector<string>& d) {
+        int n=q.size();
+        int m=d.size();
+        set<string> st(d.begin(),d.end());
 
+        vector<string> ans;
         for(int i=0;i<n;i++){
-            string curr=queries[i];
-            //edit
-            for(int j=0;j<m;j++){
-                string d=dictionary[j];
-                if(curr.length()!=d.length()) continue;
-                int diff=0;
-                for(int k=0;k<curr.length();k++){
-                    if(curr[k]!=d[k]) diff++;
-                    if(diff>2) break;
-                }
-                if(diff<=2){
-                    ans.push_back(curr);
-                    break;
+            string curr=q[i];
+            if(st.find(curr)!=st.end()) ans.push_back(curr);
+            else{
+                for(int j=0;j<m;j++){
+                    string s=d[j];
+                    int dif=0;
+                    for(int k=0;k<curr.length();k++) if(curr[k]!=s[k]) dif++;
+                    if(dif<=2  && (ans.size()==0 || ans[ans.size()-1]!=curr)) ans.push_back(curr);
                 }
             }
-            
-            
         }
         return ans;
     }
