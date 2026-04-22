82fe1104e879b855eab072fed4c2fac7726b7fa4
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-04-22 20:28:33 +0530

    Time: 15 ms (39.04%), Space: 18.9 MB (43.32%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -3,20 +3,24 @@ public:
     vector<string> twoEditWords(vector<string>& q, vector<string>& d) {
         int n=q.size();
         int m=d.size();
-        set<string> st(d.begin(),d.end());
 
         vector<string> ans;
         for(int i=0;i<n;i++){
             string curr=q[i];
-            if(st.find(curr)!=st.end()) ans.push_back(curr);
-            else{
-                for(int j=0;j<m;j++){
-                    string s=d[j];
-                    int dif=0;
-                    for(int k=0;k<curr.length();k++) if(curr[k]!=s[k]) dif++;
-                    if(dif<=2  && (ans.size()==0 || ans[ans.size()-1]!=curr)) ans.push_back(curr);
-                }
+        
+            for(int j=0;j<m;j++){
+                string s=d[j];
+                int dif=0;
+                for(int k=0;k<curr.length();k++){
+                   if(curr[k]!=s[k]) dif++; 
+                   if(dif>2) break;
+                } 
+                if(dif<=2){
+                   ans.push_back(curr); 
+                   break;
+                } 
             }
+            
         }
         return ans;
     }
