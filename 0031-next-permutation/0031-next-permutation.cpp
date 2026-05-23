a5a2ff44fcfea889a357ed197b4a7caa33d97089
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-05-24 01:12:02 +0530

    Time: 3 ms (2.25%), Space: 15.8 MB (32.18%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+
+        int maxe=nums[n-1];
+        vector<int> a;
+        a.push_back(maxe);
+        bool check=false;
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<maxe){
+                check=true;
+                a.push_back(nums[i]);
+                sort(a.begin(),a.end());
+                int k=0;
+                while(k<a.size()){
+                    if(a[k]>nums[i]) {nums[i]=a[k];a[k]=0;break;}
+                    k++;
+                }
+                sort(a.begin(),a.end());
+                cout<<a.size()<<a[1];
+                k=1;
+                while(k<a.size()){
+                    nums[i+k]=a[k];
+                    k++;
+                }
+                
+                break;
+            }
+            maxe=max(maxe,nums[i]);
+            a.push_back(nums[i]);
+        }
+
+        if(!check) reverse(nums.begin(),nums.end());
+    }
+};
