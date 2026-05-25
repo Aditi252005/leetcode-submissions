255c3d82890a6fa79c84d50b53745450f6f825f4
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-05-25 20:26:26 +0530

    Time: 17 ms (10.95%), Space: 22.9 MB (45.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -5,21 +5,19 @@ public:
         sort(piles.begin(),piles.end());
         int lo=1;
         int hi=piles[n-1];
-        int ans=piles[n-1];
-        while(lo<hi){
-            int mid= lo+(hi-lo)/2;
-            int k=0;
-            for(int i=0;i<n;i++){
-                if(piles[i]%mid==0) k=k+piles[i]/mid;
-                else k=k+ (piles[i]/mid) +1;
-            }
-            if(k<=h){   //satisfy, mid dec
-                hi=mid; 
-                ans=min(ans,mid);
-            }
-            else lo=mid+1;
 
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            int a=0;
+            for(int i=0;i<n;i++){
+                if(piles[i]%mid==0) a=a+piles[i]/mid;
+                else a=a+piles[i]/mid +1;                
+                if(a>h) break;
+            }
+            if(a<=h) hi=mid;
+            else lo=mid+1;
         }
-        return ans;
+
+        return lo;
     }
 };
