1522a68f354eee3d8ce9b738e8598b2fe1952523
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-06-07 20:48:03 +0530

    Time: 4 ms (22.97%), Space: 12.8 MB (60.74%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,22 +4,22 @@ public:
         int n=nums1.size();
         int m=nums2.size();
 
-        stack<int> st;
+        vector<int> ans(n,-1);
         map<int,int> mp;
+        stack<int> st;
+        for(int i=0;i<n;i++) mp[nums1[i]]=i;
 
         for(int i=0;i<m;i++){
+
             while(!st.empty() && nums2[i]>st.top()){
-                mp[st.top()]=nums2[i];
+                if(mp.find(st.top())!=mp.end()){
+                    ans[mp[st.top()]]=nums2[i];
+                }
                 st.pop();
             }
+
             st.push(nums2[i]);
         }
-
-        vector<int> ans(n,-1);
-        for(int i=0;i<n;i++){
-            if(mp.find(nums1[i])!=mp.end()) ans[i]=mp[nums1[i]];
-        }
-
         return ans;
     }
 };
