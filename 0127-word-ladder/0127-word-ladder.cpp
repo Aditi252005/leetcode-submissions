959ba7accbf381dec9316cb77df0b6be00e433a9
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-06-22 21:27:23 +0530

    Time: 310 ms (28.29%), Space: 21.5 MB (29.14%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    int ladderLength(string b, string e, vector<string>& wordList) {
-        int n=wordList.size();
-        set<string> st(wordList.begin(),wordList.end());
-        set<string> vis;
+    int ladderLength(string b, string e, vector<string>& w) {
+        int n=b.length();
+        int m=w.size();
+        set<string> st(w.begin(),w.end());
+        if(st.find(e)==st.end()) return 0;
+
         queue<pair<string,int>> q;
         q.push({b,1});
-       
 
         while(!q.empty()){
-            string curr=q.front().first;
-            int steps=q.front().second;
+            auto[s,steps]=q.front();
             q.pop();
 
-            if(vis.count(curr)) continue;
-            vis.insert(curr);
-            if(curr==e) return steps;
-            
-
-            string temp=curr;
-            for(int j=0;j<temp.length();j++){
-                for(int i=0;i<26;i++){
-                    temp[j]='a'+i;
-                    if(st.find(temp)!=st.end() && !vis.count(temp)) q.push({temp,steps+1});
-                    
-                }
-                temp=curr;
-            }
+            if(s==e) return steps;
            
+            for(int i=0;i<s.size();i++){
+                char orig = s[i];
+
+                for(char ch='a'; ch<='z'; ch++){
+                    s[i] = ch;
+
+                    if(st.count(s)){
+                        q.push({s,steps+1});
+                        st.erase(s);   
+                    }
+                }
+
+                s[i] = orig;
+            }
         }
         return 0;
     }
