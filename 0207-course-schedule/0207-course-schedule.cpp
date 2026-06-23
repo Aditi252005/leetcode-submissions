9e0462466775fc10a5b13b66f292b35aa1210783
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-06-23 21:02:15 +0530

    Time: 4 ms (63.67%), Space: 19.4 MB (54.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& pre) {
-        int n=pre.size();
-        map<int,vector<int>> mp;
-        for(int i=0;i<n;i++){
-            mp[pre[i][0]].push_back(pre[i][1]);
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        int m=pre.size();
+        vector<vector<int>> adj(n);
+        vector<int> ind(n,0);
+        for(int i=0;i<m;i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+            ind[pre[i][0]]++;
         }
-        
 
+        queue<int> q;
+        vector<int> ans;
         for(int i=0;i<n;i++){
-            queue<int> q;
-            q.push(pre[i][1]);
-            set<int> vis;
-           
-            while(!q.empty()){
-                int p=q.front();
-                q.pop();
-                if(vis.find(p)!=vis.end()) continue;
-                vis.insert(p);
-                for(auto it:mp[p]) {
-                    if(it==pre[i][0])return false;
-                    q.push(it);
-                }
-                
+            if(ind[i]==0) {
+                q.push(i);
             }
         }
-        return true;
+
+        
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+
+            ans.push_back(curr);
+            for(auto nbr:adj[curr]){
+                ind[nbr]--;
+                if(ind[nbr]==0) q.push(nbr);
+            }
+        }
+
+        if(ans.size()==n) return true;
+        return false;
     }
 };
