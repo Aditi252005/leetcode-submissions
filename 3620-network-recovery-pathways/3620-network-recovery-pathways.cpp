d49897e8732f7366f8e10ce91948173339cd45f2
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-03 14:01:54 +0530

    Time: 649 ms (21.73%), Space: 377.2 MB (38.34%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -1,41 +1,59 @@
 class Solution {
 public:
-    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+    bool check(int w,vector<vector<pair<int,int>>>&adj,vector<bool>& online, long long k){
         int n=online.size();
-        
 
-        vector<vector<pair<int,int>>> adj(n);
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            int wt=edges[i][2];
-            adj[u].push_back({v,wt});
-        }
+        priority_queue<pair<long long,int>,
+        vector<pair<long long,int>>,
+        greater<pair<long long,int>>> pq;
+        pq.push({0,0});
 
-       
-        int maxMinEdge=-1;
-        priority_queue<tuple<int,int,long long>> pq;
-        if(online[0]) pq.push({INT_MAX,0,0});
-        // minc,node,dist
+        vector<long long> dis(n,LLONG_MAX);
+        dis[0]=0;
 
         while(!pq.empty()){
-            auto [minc,node,dist]=pq.top();
+            auto [tc,node]=pq.top();
             pq.pop();
-            
-            if(node==n-1){
-                maxMinEdge=max(maxMinEdge,minc);
-                break;  //why break?
-            }
 
-            for(auto neighbour:adj[node]){
-                int nnode=neighbour.first;
-                int nt=neighbour.second;
-                if(dist+nt<=k && online[nnode]){
-                    pq.push({min(minc,nt),nnode,dist+nt}); 
+            if(dis[node]<tc) continue;
+
+            for(auto [nbr,c]:adj[node]){
+                if(!online[nbr] || c<w) continue;
+                if(dis[nbr]>tc+c && tc+c<=k){
+                    dis[nbr]=tc+c;
+                    pq.push({tc+c,nbr});
                 }
             }
         }
 
-       return maxMinEdge;
+        return dis[n-1]<=k;
+
+    }
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        int n=online.size();
+        
+        vector<vector<pair<int,int>>> adj(n);
+        int maxew=0;
+        for(auto e:edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            maxew=max(maxew,e[2]);
+        }
+
+       
+        int lo=0;
+        int hi=maxew;
+        int ans=-1;
+
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+
+            if(check(mid,adj,online,k)){
+                ans=mid;
+                lo=mid+1;
+            }
+            else hi=mid-1;
+        }
+
+        return ans;
     }
 };
