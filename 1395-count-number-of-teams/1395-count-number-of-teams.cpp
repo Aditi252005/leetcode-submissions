1707af7fe03ab44480c35b30ca8a16269c1ae1d0
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-09 20:22:39 +0530

    Time: 1526 ms (21.26%), Space: 12.7 MB (45.44%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int count=0;
+      
+        int n=rating.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+
+                if(rating[i]<rating[j]){
+                    for(int k=j+1;k<n;k++){
+                        if(rating[k]>rating[j]) count++;
+                    }
+                }
+                else if(rating[i]>rating[j]){
+                    for(int k=j+1;k<n;k++){
+                        if(rating[j]>rating[k]) count++;
+                    }
+                }
+            }
+        }
+      return count;
+    }
+};
