cfd79c8126d58ed289713f6dd2dba0ea54dc681f
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-23 20:53:57 +0530

    Time: 381 ms (16.26%), Space: 8.6 MB (78.61%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    bool check(int i,int j,string s,vector<vector<int>>&dp){
-        int n=s.length();
-        if(i>=j) return true;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s[i]!=s[j]) return dp[i][j]=false;
-        return dp[i][j]=check(i+1,j-1,s,dp);
-        
+    bool isPali(int i,int j,string&s){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
     }
     int countSubstrings(string s) {
         int n=s.length();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
 
         int ans=0;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(s[i]==s[j] && check(i,j,s,dp)) ans+=1; 
+            for(int j=n-1;j>=i;j--){
+                if(isPali(i,j,s)) ans++;
             }
         }
         return ans;
