3c195921ab3535f76ed96422acee25ef4460a4d2
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-26 21:11:37 +0530

    Time: 7 ms (34.57%), Space: 26.4 MB (25.06%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    int f(int l,TreeNode* root,map<TreeNode*,int>& dp){
+    int f(TreeNode*root,map<TreeNode*,int>&dp){
         if(!root) return 0;
-        if(dp.find(root)!=dp.end()) return dp[root];
+        if(dp.count(root)) return dp[root];
 
-        int a=0,b=0,c=0,d=0;
-        if(root->left){
-           a=f(l+2,root->left->left,dp);  
-           b=f(l+2,root->left->right,dp);
-        } 
-       
-        if(root->right){
-          c=f(l+2,root->right->left,dp);  
-          d=f(l+2,root->right->right,dp);
-        } 
-       
+        int take= root->val;
+        if(root->left) take+= f(root->left->left,dp)+f(root->left->right,dp);
+        if(root->right) take+= f(root->right->left,dp)+f(root->right->right,dp);
 
-        int take= root->val+a+b+c+d;
-        int skip=f(l+1,root->left,dp)+f(l+1,root->right,dp);
+        int skip= f(root->left,dp)+f(root->right,dp);
 
-        return dp[root]= max(take,skip);
-
+        return dp[root]=max(take,skip);
     }
-    
     int rob(TreeNode* root) {
-      map<TreeNode*,int> dp;
-        return f(1,root,dp);
+        
+        map<TreeNode*,int> dp;
+        return f(root,dp);
     }
 };
