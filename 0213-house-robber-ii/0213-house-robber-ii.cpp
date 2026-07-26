02acb75ed0b233118ac76e7bdae7bb4877e8cdf7
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-26 20:27:00 +0530

    Time: 0 ms (100%), Space: 10.9 MB (37.62%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int f(int i,int n,vector<int>&nums,vector<int>&dp){
-        if(i>n) return 0;
-        if(i==n) return nums[i];
+        if(i>=n) return 0;
         if(dp[i]!=-1) return dp[i];
 
-        int take=nums[i]+f(i+2,n,nums,dp);
-        int notTake=f(i+1,n,nums,dp);
-
-        return dp[i]= max(take,notTake);
+        return dp[i]=max(nums[i]+f(i+2,n,nums,dp),f(i+1,n,nums,dp));
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-        return max(f(0,n-2,nums,dp1),f(1,n-1,nums,dp2));
+        vector<int> dp1(n+1,-1);
+        vector<int> dp2(n+1,-1);
+        return max(f(0,n-1,nums,dp1),f(1,n,nums,dp2));
     }
 };
