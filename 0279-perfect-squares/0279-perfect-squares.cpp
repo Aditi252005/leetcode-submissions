2c35eca8b6f898af012cf4e7330cdf3bb7156e84
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-28 21:02:09 +0530

    Time: 82 ms (34.49%), Space: 38.6 MB (18.37%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dp=vector<int>(10002,-1); 
+    int numSquares(int n) {
+        if(n==0) return 0;
+        if(dp[n]!=-1) return dp[n];
+
+        int ans=INT_MAX;
+        for(int i=1;i*i<=n;i++){
+            ans=min(ans,1+numSquares(n-i*i));
+        }
+
+        return dp[n]=ans;
+    }
+};
