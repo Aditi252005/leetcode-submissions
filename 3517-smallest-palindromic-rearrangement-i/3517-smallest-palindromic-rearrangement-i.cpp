cd832ff8ee24f4a0ef6058b2320d5cda7adc5df8
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-28 12:48:24 +0530

    Time: 25 ms (76.26%), Space: 60.9 MB (78.46%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,32 +2,23 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n=s.length();
-        vector<int> v(26,0);
-        for(auto c:s) v[c-'a']++;
 
+        vector<int> v(26,0);
         string ans(n,' ');
-        int j=0,k=n-1;
+        for(auto ch:s){
+            v[ch-'a']++;
+        }
+        
+        int j=0;
         for(int i=0;i<26;i++){
             while(v[i]){
-                if(v[i]%2==0){
-                    ans[j]=i+'a';
-                    ans[k]=i+'a';
-                    v[i]-=2;
+                if(v[i]%2) {ans[n/2]='a'+i;v[i]--;}
+                else{
+                    ans[j]='a'+i;
+                    ans[n-j-1]='a'+i;
                     j++;
-                    k--;
-                }else{
-                    if(v[i]==1) {ans[(j+k)/2]=i+'a';v[i]--;}
-                    else{
-                        ans[j]=i+'a';
-                        ans[k]=i+'a';
-                        ans[(j+k)/2]=i+'a';
-                        v[i]-=3;
-                        j++;
-                        k--;
-                    }
+                    v[i]-=2;
                 }
-                // j++;
-                // k--;
             }
         }
         return ans;
