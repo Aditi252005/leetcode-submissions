ca1444b2ac450780121ef4d4f0e4a8273ac82e71
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-07-30 13:35:14 +0530

    Time: 2085 ms (5%), Space: 115.9 MB (5.14%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    bool check(int i,int j,string&s){
+    bool isPali(int i,int j,string&s,vector<vector<int>>&dp){
+        if(dp[i][j]!=-1) return dp[i][j];
         while(i<j){
-            if(s[i]!=s[j]) return false;
+            if(s[i]!=s[j]) return dp[i][j]=false;
             i++;
             j--;
         }
-        return true;
+        return dp[i][j]=true;
     }
-    int f(int i,string&s,vector<int>&dp){
+    int f(int i,int j,string&s,vector<vector<int>>&dp1,vector<vector<int>>&dp2){
         int n=s.length();
-        if(i==n || check(i,n-1,s)) return 0;
-        if(dp[i]!=-1) return dp[i];
+        if(i==n || j==n) return 0;
+        if(dp2[i][j]!=-1) return dp2[i][j];
 
-        int mini=1e5;
-        for(int j=i;j<n;j++){
-            if(check(i,j,s)) mini=min(mini,1+f(j+1,s,dp));
+        int ans=INT_MAX;
+        for(int k=j;k<n;k++){
+            if(isPali(i,k,s,dp1)){
+                ans=min(ans,1+f(k+1,k+1,s,dp1,dp2));
+            }
         }
-        return dp[i]=mini;
+        return dp2[i][j]=ans;
     }
     int minCut(string s) {
         int n=s.length();
-        vector<int> dp(n+1,-1);
+        vector<vector<int>> dp1(n+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp2(n+1,vector<int>(n+1,-1));
 
-        return f(0,s,dp);
+        return f(0,0,s,dp1,dp2)-1;
     }
 };
