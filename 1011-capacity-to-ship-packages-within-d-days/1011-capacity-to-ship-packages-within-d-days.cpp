9404289e7bb16be04a3cdd53fcf407c9737bb8ee
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-08-01 20:59:46 +0530

    Time: 10 ms (70.21%), Space: 35.1 MB (20.13%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         int n=weights.size();
-        
+
         int lo=*max_element(weights.begin(),weights.end());
         int hi=accumulate(weights.begin(),weights.end(),0);
 
         while(lo<hi){
-            int mid= lo+(hi-lo)/2;
-            int d=1;
+            int d=days;
+            int mid=lo+(hi-lo)/2;
             int s=0;
-            bool check=false;
+            bool check=true;
 
             for(int i=0;i<n;i++){
+                if(d==0) {check=false;break;}
                 if(s+weights[i]<=mid) s+=weights[i];
-                else {s=weights[i];d++;}
-                if(d>days){
-                    break;
-                }
+                else {d--;s=weights[i];}
             }
-            if(d<=days) hi=mid;
+
+            //cout<<lo<<mid<<hi<<check<<endl;
+            if(check && d>0) hi=mid;
             else lo=mid+1;
         }
         return lo;
