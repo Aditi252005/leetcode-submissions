6bf1c33773d38ae72d2f4096ca93120cf1f17f13
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-08-02 13:23:04 +0530

    Time: 0 ms (100%), Space: 10.8 MB (38.15%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int f(vector<int>&piles,bool turn,int i,int j,vector<vector<int>>&dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+    int f(vector<int>&piles,int i,vector<int>&dp){
+        int n=piles.size();
+        if(i>n-1) return 0;
+        if(dp[i]!=-1) return dp[i];
 
-        if(turn){
-            int op1=piles[i]+f(piles,false,i+1,j,dp);
-            int op2=piles[j]+f(piles,false,i,j-1,dp);
-            return dp[i][j]=max(op1,op2);
-        }else{
-            int op1=piles[i]+f(piles,true,i+1,j,dp);
-            int op2=piles[j]+f(piles,true,i,j-1,dp);
-            return dp[i][j]=max(op1,op2);
-        }
+        int op1=piles[i]+f(piles,i+2,dp);
+        int op2=f(piles,i+1,dp);
+        return dp[i]=max(op1,op2);
     }
     bool stoneGame(vector<int>& piles) {
         int n=piles.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        vector<int> dp(n+1,-1);
 
         int tot=accumulate(piles.begin(),piles.end(),0);
-        int a=f(piles,true,0,n-1,dp);
+        int a=f(piles,0,dp);
         if(a>tot/2) return true;
         return false;
 
