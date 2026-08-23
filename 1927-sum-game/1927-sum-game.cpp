bd0a1f3efdd5516ab3cbf2743bc66e03b30cadde
Aditi Singh <168583064+Aditi252005@users.noreply.github.com>
2026-08-23 11:06:46 +0530

    Time: 6 ms (25.41%), Space: 13.9 MB (92.07%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n=num.length();
+        int q1=0,q2=0,s1=0,s2=0;
+
+        for(int i=0;i<n;i++){
+            if(i<n/2){
+                if(num[i]=='?') q1++;
+                else s1+=num[i]-'0';
+            }else{
+                if(num[i]=='?') q2++;
+                else s2+=num[i]-'0';
+            }
+        }
+
+        return (s1-s2)*2 != (q2-q1)*9;
+    }
+};
